3be5016aca10476b41667ec6627b2ac733e00fee
Szikszai Gusztáv <guszti5@hotmail.com>
2016-07-13 08:16:48 +0200

    Add more specs.

--- spec/cr-corm_spec.cr
+++ spec/cr-corm_spec.cr
@@ -2,21 +2,39 @@ require "./spec_helper"
 
 describe Corm do
   describe "#select" do
-    it "should raise error if the table is not defined" do
-      expect_raises do
-        Corm.select("id")
+    context "no table defined" do
+      it "should raise error" do
+        expect_raises(Corm::NoTableDefined) { Corm.select("id") }
+      end
+    end
+  end
+
+  describe "#table" do
+    context "already set" do
+      it "should raise error" do
+        expect_raises(Corm::TableAlreadyDefined) do
+          Corm.table("users")
+              .table("projects")
+        end
       end
     end
   end
 
   it "build sql" do
-    sql = Corm.select("id", "name")
+    sql = Corm.table("users")
+              .select("id", "name")
               .to_sql
 
     sql[0].should eq(%(SELECT "users"."id", "users"."name" FROM "users"))
   end
 
   describe "SQL functions" do
+    context "no table defined" do
+      it "should raise error" do
+        expect_raises(Corm::NoTableDefined) { Corm.count("id") }
+      end
+    end
+
     it "should build sql functions into select queries" do
       sql = Corm.table("users")
                 .count("id")
@@ -33,7 +51,9 @@ describe Corm do
                 .where({"id" => 10})
                 .to_sql
 
-      sql[0].should eq(%(SELECT "users"."id", "users"."name" FROM "users" WHERE "users"."id" = $1))
+      sql[0].should eq(%(SELECT "users"."id", "users"."name" \
+                         FROM "users" \
+                         WHERE "users"."id" = $1))
       sql[1].should eq [10]
     end
   end
